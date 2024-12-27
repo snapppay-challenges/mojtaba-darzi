fa058193c68d5f2bb288fdc40358c8fc8489cdd4
x <x@Apples-MacBook-Air.local>
2024-12-28 00:09:54 +0330

    refactore(components): modify style & html

--- src/components/home/load-more-button.tsx
+++ src/components/home/load-more-button.tsx
@@ -5,9 +5,9 @@ type LoadMoreProps = { onClick: () => void, loading: boolean }
 const LoadMoreButton = memo(({ onClick, loading }: LoadMoreProps) => (
     <button
         onClick={onClick}
-        className="border border-2 border-blue-500 text-blue-500 px-4 py-2 rounded-full mt-4 mx-auto 
-            block hover:bg-blue-500 hover:text-white disabled:border-gray-300 disabled:bg-white 
-            disabled:text-gray-300 disabled:cursor-not-allowed"
+        className="border border-2 border-transparent hover:border-gray-900 bg-gray-900 hover:bg-white 
+            text-white hover:text-gray-900  px-4 py-2 rounded-full mt-4 mx-auto block disabled:border-gray-300 
+            disabled:bg-white disabled:text-gray-300 disabled:cursor-not-allowed"
         disabled={loading}
     >
         Load More
