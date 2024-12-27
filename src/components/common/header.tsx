8db42365b21b6f1c7120b9e8586dc32f62e59ed4
x <x@Apples-MacBook-Air.local>
2024-12-27 23:37:54 +0330

    perf(component): add aria-label on header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,4 +1,3 @@
-import { useCallback } from "react"
 import { SlArrowLeft, SlHome } from "react-icons/sl"
 import { useNavigate } from "react-router-dom"
 
@@ -6,19 +5,14 @@ type HeaderProps = { title: string, back?: boolean, home?: boolean }
 
 const Header = ({ title = "", back = true, home = true }: HeaderProps) => {
     const navigate = useNavigate()
-    const goBack = useCallback(() => {
-        navigate(-1)
-    }, [])
-
-    const goHome = useCallback(() => {
-        navigate("/")
-    }, [])
+    const goBack = () => navigate(-1)
+    const goHome = () => navigate("/")
 
     return <div className="w-full flex h-16 justify-center items-center relative">
-        {back && <button className="w-16 h-16 flex justify-center items-center absolute top-0 left-0" onClick={goBack}>
+        {back && <button className="w-16 h-16 flex justify-center items-center absolute top-0 left-0" onClick={goBack} aria-label="back">
             <SlArrowLeft size={24} />
         </button>}
-        {home && <button className="w-16 h-16 flex justify-center items-center absolute top-0 right-0" onClick={goHome}>
+        {home && <button className="w-16 h-16 flex justify-center items-center absolute top-0 right-0" onClick={goHome} aria-label="home">
             <SlHome size={24} />
         </button>}
         <h1 className="text-xl sm:text-2xl font-semibold">{title}</h1>
